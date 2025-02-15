447bf4dea9e013a20ef390b6a51d0bf1f4bb85bb
David lopez <davidlopezing.1@gmail.com>
2025-02-15 10:36:59 -0500

    EjercicioTaller v4

---
+++ src/excercise4.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class excercise4 {
+
+    public static String invertirCadena(String cadena) {
+
+        String invert = "";
+
+        for(int i = cadena.length() -1; i >= 0; i--){
+            invert += cadena.charAt(i);
+            System.out.println(invert);
+        }
+        return invert;
+    }
+
+    public static void main(String[] args) {
+        String cadena = "Hola mundo";
+
+
+        System.out.println("El texto invertido es: " + invertirCadena(cadena));
+    }
+
+}
