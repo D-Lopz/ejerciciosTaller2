14a4d825b8404af585788dc74d45fb6ad2428700
David lopez <davidlopezing.1@gmail.com>
2025-02-15 21:53:29 -0500

    EjercicioTaller v5

---
+++ src/excercise5.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+
+public class excercise5 {
+    public static List<Integer> eliminarDuplicados(List<Integer> arr) {
+        Set<Integer> set = new LinkedHashSet<>(arr);
+        return new ArrayList<>(set);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numeros = List.of(1, 2, 2, 3, 4, 4, 5);
+
+        System.out.println(eliminarDuplicados(numeros));
+    }
+}
